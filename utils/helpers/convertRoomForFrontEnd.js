501563cd19ba9ae2926b58083c241afa29b82bd9
tektista <jnl.bautista.cs@gmail.com>
2023-03-14 18:01:24 +0000

    add convertRoomForFrontend

---
+++ utils/helpers/convertRoomForFrontEnd.js
@@ -0,0 +1,17 @@
+const convertRoomForFrontEnd = (roomObj) => {
+  const room = { ...roomObj };
+
+  if (room.hasOwnProperty("is_furnished")) {
+    room.is_furnished == 1
+      ? (room.is_furnished = "Yes")
+      : (room.is_furnished = "No");
+  }
+
+  if (room.hasOwnProperty("room_size")) {
+    room.room_size = room.room_size === 0 ? "Single Room" : "Double Room";
+  }
+
+  return room;
+};
+
+module.exports = convertRoomForFrontEnd;
