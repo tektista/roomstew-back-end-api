17c30462abdcdf87224acc6ac7fafa5219725646
tektista <jnl.bautista.cs@gmail.com>
2023-04-11 12:54:41 +0100

    add comment

--- models/db.js
+++ models/db.js
@@ -1,6 +1,9 @@
 const mysql = require("mysql2/promise");
 const config = require("../utils/config");
 
+/*
+create a connection to the db using config
+*/
 const pool = mysql.createPool({
   //NOTE: for some reason "config.USER" doesn't work, but "username" does
   host: config.HOST,
